3232748c3f1b5d625c0150432f7ae3ddeb2b9c07
CalkaRob <calka.rob@gmail.com>
2019-11-03 20:45:43 +0100

    up

---
+++ build/precache-manifest.b0a3467ed1edb6779efdad9b215dc302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74613f6989b973d41bc426656023c9be",
+    "url": "/coralinio-preview/build/index.html"
+  },
+  {
+    "revision": "c30eec3065751c9caecb",
+    "url": "/coralinio-preview/build/static/css/2.db237fb7.chunk.css"
+  },
+  {
+    "revision": "475920bfb97bf9b1e7f2",
+    "url": "/coralinio-preview/build/static/css/main.996e407c.chunk.css"
+  },
+  {
+    "revision": "c30eec3065751c9caecb",
+    "url": "/coralinio-preview/build/static/js/2.85fddc35.chunk.js"
+  },
+  {
+    "revision": "475920bfb97bf9b1e7f2",
+    "url": "/coralinio-preview/build/static/js/main.06e91096.chunk.js"
+  },
+  {
+    "revision": "11b2f30a6c6911db9907",
+    "url": "/coralinio-preview/build/static/js/runtime-main.1955a6e3.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coralinio-preview/build/precache-manifest.13453a1e4a71d22378e4b8fcf66ee6a6.js"
+  "/coralinio-preview/build/precache-manifest.b0a3467ed1edb6779efdad9b215dc302.js"
 );
 
 self.addEventListener('message', (event) => {
