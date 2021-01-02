342d635d2998e260a38de04149e3f377dba9e438
CalkaRob <calka.rob@gmail.com>
2021-01-02 15:47:01 +0100

    up fs

---
+++ precache-manifest.47d97b8383fa59c9d99da937a7f0eedf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a9882b78ee95509a239d26172b171e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72969ae6605e8eecccc9",
+    "url": "./static/css/2.db237fb7.chunk.css"
+  },
+  {
+    "revision": "4e4ed677dedba7164132",
+    "url": "./static/css/main.5a4c2824.chunk.css"
+  },
+  {
+    "revision": "72969ae6605e8eecccc9",
+    "url": "./static/js/2.ff465100.chunk.js"
+  },
+  {
+    "revision": "8ec8e4cfc21fb9652d7367fcdfc0293c",
+    "url": "./static/js/2.ff465100.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e4ed677dedba7164132",
+    "url": "./static/js/main.1777279f.chunk.js"
+  },
+  {
+    "revision": "3751af15ce361ab61bb3",
+    "url": "./static/js/runtime-main.9e1856c5.js"
+  }
+]);
